c3ddd3bf58b1f75e5c62ca094a922cb6a58ad9c9
sid <sid@portuus.de>
2025-05-17 01:38:15 +0200

    update flake.lock

--- modules/nixos/common/nix.nix
+++ modules/nixos/common/nix.nix
@@ -16,42 +16,12 @@
     #   }
     # ];
 
-    #  auto run garbage collection when out of space
-    extraOptions = ''
-      min-free = ${toString (100 * 1024 * 1024)}
-      max-free = ${toString (1024 * 1024 * 1024)}
-    '';
-
-    # auto run garbage collection once a week
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-
-    # flake alias for nix-core
-    registry = {
-      core = {
-        from = {
-          id = "core";
-          type = "indirect";
-        };
-        to = {
-          owner = "sid115";
-          repo = "nix-core";
-          type = "github";
-        };
-      };
-    };
-
     settings = {
       # binary caches
       substituters = [
-        "https://nix-community.cachix.org"
         "https://cache.portuus.de"
       ];
       trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.portuus.de:INZRjwImLIbPbIx8Qp38gTVmSNL0PYE4qlkRzQY2IAU="
       ];
     };
