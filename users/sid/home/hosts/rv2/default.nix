eac49a5726f2ec5280b8fe69d2e8575fd0530e41
sid <sid@portuus.de>
2025-06-17 13:48:20 +0200

    rv2 display config

--- users/sid/home/hosts/rv2/default.nix
+++ users/sid/home/hosts/rv2/default.nix
@@ -2,9 +2,9 @@
   imports = [ ../../hyprland ];
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "HDMI-A-1, 1920x1200, 0x0, 1"
+    "DVI-D-1, 1920x1200, 0x0, 1"
     "DVI-I-1, 1280x1024, 1920x0, 1, transform, 1"
   ];
 
-  programs.waybar.settings.mainBar.output = "HDMI-A-1";
+  programs.waybar.settings.mainBar.output = "DVI-D-1";
 }
