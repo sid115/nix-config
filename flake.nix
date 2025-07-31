622daa2f9dd6d64a381042a2107116f6044670ea
sid <sid@portuus.de>
2025-07-31 21:38:25 +0200

    running on nixpkgs/pull/427905

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/pull/427905/head"; # https://github.com/NixOS/nixpkgs/issues/426401
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-old-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-old-old-stable.url = "github:nixos/nixpkgs/nixos-24.05";
