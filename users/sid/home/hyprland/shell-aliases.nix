6b5c5a143cd0c5aad7b1bd37d25187854313295f
sid <sid@portuus.de>
2025-11-27 22:12:20 +0100

    added ff alias

--- users/sid/home/hyprland/shell-aliases.nix
+++ users/sid/home/hyprland/shell-aliases.nix
@@ -3,6 +3,7 @@
 {
   home.shellAliases = {
     bt = "bluetoothctl";
+    ff = "find . -type f -name";
     dd-iso = "dd bs=4M status=progress oflag=sync";
     nc-sync = "nextcloud-sync-all";
     synapse_change_display_name = "${pkgs.core.synapse_change_display_name}/bin/synapse_change_display_name -t $(${pkgs.pass}/bin/pass sid.ovh/matrix/access-token) -r sid.ovh";
