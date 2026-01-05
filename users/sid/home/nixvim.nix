58debda939b49857c772338dc90e44957d28246d
sid <sid@portuus.de>
2026-01-05 01:55:47 +0100

    update core input

--- users/sid/home/nixvim.nix
+++ users/sid/home/nixvim.nix
@@ -28,13 +28,11 @@
       render-markdown = {
         enable = true;
         settings = {
-          file_types = [
-            "Avante"
-          ];
+          # file_types = [
+          #   "Avante"
+          # ];
         };
       };
-      which-key.enable = true;
-      # cmp-nvim-ultisnips.enable = true;
     };
   };
 }
